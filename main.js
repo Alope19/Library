94993db6529b34f8828a93b3a7d6762cb677d174
Anthony Lopez <anthony.dv.lopez@gmail.com>
2023-08-20 23:20:44 -0500

    Add logic for saving user input into object
    also made it so user input is stored into array

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
+function Book(title,author,pages,read){
+    this.title = title,
+    this.author = author
+    this.pages = pages,
+    this.read = read
+}
+let bookshelf =[]
 const addBox =  document.querySelector('.addbox')
+const form = document.getElementById("addform")
 
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let pages = document.getElementById('pages').value
+    let read = document.getElementById('read').checked
+
+    bookshelf.push(new Book(title,author,pages,read))
+
+    addBox.style.visibility = 'hidden'
+
+    console.log(bookshelf)
+
+})
+
+//Makes it so clicking on add book opens the form to add a book
 document.querySelector(".addbutton").addEventListener('click',()=>{
     addBox.style.visibility = 'visible'
 })
